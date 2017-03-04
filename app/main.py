3e9c6b4426aea3e081bd72986ebb5abd2b997a88
Joseph Woolfrey <joseph.woolfrey@gmail.com>
2017-03-04 14:39:41 -0800

    coordinate
    fixed coordinate lists

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ def snake_length(snake):
     return len(snake.coords)
 
 def snake_direction(snake):
-    sdir = [snake.coords[0].x - snake.coords[1].x, snake.coords[0].y - snake.coords[1].y]
+    sdir = [snake['coords'][0][0] - snake['coords'][1][0], snake['coords'][0][1] - snake['coords'][1][1]]
     return {
         [0,0]:'FIRSTMOVE',
         [0,1]:'down',
